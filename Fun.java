f01b9b90223474a5a2c5892fde440f6d78aa3253
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-09-28 10:28:30 +0530

    Add files via upload

---
+++ Fun.java
@@ -0,0 +1,12 @@
+public class Fun{
+              public static void add(){
+
+            System.out.println("Print Message");
+        }
+        public static void main(String[] args )
+{
+  add();
+
+}   
+ }
+
